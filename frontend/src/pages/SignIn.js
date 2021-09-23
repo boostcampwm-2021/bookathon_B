736c71d8268d8ded60c20f10abdd749041961f35
Harry <haesoo9410@gmail.com>
2021-09-23 22:34:22 +0900

    :lipstick: 가입 페이지 레이아웃 추가
    가입 페이지 레이아웃을 추가함

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import MainLogo from "../components/MainLogo";
+
+const MainForm = styled.form`
+  width: 300px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  input {
+    height: 40px;
+    padding: 10px;
+    margin-bottom: 15px;
+    background-color: black;
+    color: white;
+    border: solid 1px #6f737a;
+    border-radius: 5px;
+  }
+  button {
+    height: 30px;
+    color: white;
+    border: solid 1px #6f737a;
+    border-radius: 5px;
+    background-color: #2e562f;
+  }
+`;
 
 const SignIn = () => {
-  return <div>signin</div>;
+  const [email, setEmail] = useState("");
+  const [nickName, setNickName] = useState("");
+  return (
+    <div>
+      <MainLogo />
+      <MainForm>
+        <input placeholder="Enter Email Address" />
+        <input placeholder="Enter Your Nickname" />
+        <button>Sign In Jandi-Jandi</button>
+      </MainForm>
+    </div>
+  );
 };
 
 export default SignIn;
