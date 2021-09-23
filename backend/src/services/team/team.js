dbabb6a47544a337ce087592b1d2c8e8c91478f9
이찬호 <chanholee275@gmail.com>
2021-09-23 17:38:36 +0900

    👔 스터디 그룹의 내용 변경을 위한 서비스 로직 작성
    teamId를 받아서 그 Id에 해당하는 객체의 값을 요청 바디에 있는 값으로 변경

--- backend/src/services/team/team.js
+++ backend/src/services/team/team.js
@@ -42,10 +42,21 @@ const deleteTeam = async (req, res, next) => {
 
 const updateTeam = async (req, res, next) => {
     try{
-
+        const teamId = req.params['teamId'];
+        await Team.findByIdAndUpdate(teamId,{...req.body});
+        await res.status(200).json({
+            code: '2000',
+            status: '성공 : Team 수정',
+            message: 'Team이 정상적으로 수정되었습니다.'
+        });
     }
     catch{
-        
+        console.error(err);
+        await res.status(500).json({
+            code: '5000',
+            status: '에러 : 서버 에러',
+            message : '수정 요청 처리 중 서버에서 문제가 발생했습니다.'
+        });
     }
 };
 
@@ -54,6 +65,6 @@ const searchTeams = async (req, res, next) => {
 
     }
     catch{
-        
+
     }
 };
