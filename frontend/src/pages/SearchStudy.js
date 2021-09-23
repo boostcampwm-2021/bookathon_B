9a3096b04fe3245a755cb7b183f250210e2cb271
seojin kim <001106ksj@gmail.com>
2021-09-23 21:46:58 +0900

    :lipstick: 검색 결과 컴포넌트 만들기
    버튼은 따로 컴포넌트로 뺌!

--- frontend/src/pages/SearchStudy.js
+++ frontend/src/pages/SearchStudy.js
@@ -1,12 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import { useLocation } from "react-router";
+import SearchResultCard from "../components/SearchResultCard";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const SearchStudy = () => {
   const location = useLocation();
   const query = location.search;
   const parsedQuery = new URLSearchParams(query);
   const searchWord = parsedQuery.get("q");
+
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    function getResult() {
+      const result = [
+        // 여기 나중에 API 호출하기!
+        {
+          title: "잔디잔디",
+          description: "잔디를 길러봐",
+          isPrivate: true,
+          count: 3,
+          password: "12345",
+        },
+        {
+          title: "잔디잔디잔디",
+          description: "잔디를 길러봐?",
+          isPrivate: false,
+          count: 3,
+          password: null,
+        },
+        {
+          title: "잔디잔",
+          description: "잔디를 길러봐앙",
+          isPrivate: true,
+          count: 3,
+          password: "12345",
+        },
+        {
+          title: "잔잔디디",
+          description: "잔디를 길러봐아아?",
+          isPrivate: false,
+          count: 3,
+          password: null,
+        },
+        {
+          title: "디잔디",
+          description: "잔디를 길러봐ba",
+          isPrivate: true,
+          count: 3,
+          password: "12345",
+        },
+        {
+          title: "잔잔잔",
+          description: "잔디를 길러봐bwa?",
+          isPrivate: false,
+          count: 3,
+          password: null,
+        },
+      ];
+      setSearchResult(result);
+    }
+    getResult();
+  }, []);
 
   return (
     <div style={{ marginTop: "50px", marginBottom: "50px" }}>
@@ -16,6 +78,11 @@ const SearchStudy = () => {
           {searchWord}에 대한 검색 결과입니다.
         </div>
       ) : null}
+      <CardContainer>
+        {searchResult
+          ? searchResult.map((result) => <SearchResultCard data={result} />)
+          : null}
+      </CardContainer>
     </div>
   );
 };
