7ff8b77185f497246bb82aebc5b5e314e1c727d7
gidskql6671 <gidskql6671@gmail.com>
2021-09-23 16:32:53 +0900

    :sparkles: github OAuth 로그인 구현
    추후 DB와의 연동을 통해 정보를 추가할 필요가 있다.
    현재는 말그대로 로그인만 하였다.

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+
+// env 파일의 변수들을 환경변수(process.env)로 등록해줌
+require("dotenv").config({path: path.join(__dirname, "/envs/passport.env")});
 
 const indexRouter = require('./src/routes/index');
-const usersRouter = require('./src/routes/users');
+const userRouter = require('./src/routes/user');
+const authRouter = require('./src/routes/auth');
 
 const app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret: '1q2w3e4r', resave:true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./src/configs/passport')();
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
