a804b4be12359c2da0ab0c0bbb068e4ef98784e0
이찬호 <chanholee275@gmail.com>
2021-09-23 17:24:29 +0900

    👔 스터디 그룹 삭제를 위한 teamId가 일치하는 team 삭제 구현

--- backend/src/services/team/team.js
+++ backend/src/services/team/team.js
@@ -14,20 +14,46 @@ const createTeam = async (req, res, next) => {
         await res.status(500).json({
             code: '5000',
             status: '에러 : 서버 에러',
-            message : '요청 처리 중 서버에서 문제가 발생했습니다.'
+            message : '생성 요청 처리 중 서버에서 문제가 발생했습니다.'
         });
     }
 };
 
 
 const deleteTeam = async (req, res, next) => {
-
+    try{
+        const teamId = req.params['teamId'];
+        await Team.findByIdAndDelete(teamId);
+        await res.status(200).json({
+            code: '2000',
+            status: '성공 : Team 삭제',
+            message: 'Team이 정상적으로 삭제되었습니다.'
+        });
+    }
+    catch{
+        console.error(err);
+        await res.status(500).json({
+            code: '5000',
+            status: '에러 : 서버 에러',
+            message : '삭제 요청 처리 중 서버에서 문제가 발생했습니다.'
+        });
+    }
 };
 
 const updateTeam = async (req, res, next) => {
+    try{
 
+    }
+    catch{
+        
+    }
 };
 
 const searchTeams = async (req, res, next) => {
+    try{
 
+    }
+    catch{
+        
+    }
 };
