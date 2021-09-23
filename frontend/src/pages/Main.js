3a782c7f9e4ffdc01c07d1e539a0b6522fb59873
Harry <haesoo9410@gmail.com>
2021-09-24 01:44:09 +0900

    :sparkles: 사용자의 그룹리스트 표시
    사용자 아이디에 맞게 리스트를 표시함

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MyStudyCard from "../components/MyStudyCard";
 import ProfileCard from "../components/ProfileCard";
+import styled from "styled-components";
+
+const CardSection = styled.section`
+  width: 70vw;
+  max-width: 1000px;
+  margin: 20px 100px 0 80px;
+  h2 {
+    font-weight: 600;
+    text-align: left;
+    margin-bottom: 10px;
+  }
+`;
+
+const CardList = styled.div`
+  display: inline-flex;
+
+  flex-wrap: wrap;
+`;
 
 const Main = ({ userObj }) => {
+  const [studyArray, setStudyArray] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getStudyArray = async () => {
+      let newArray;
+      try {
+        newArray = await (await fetch(`/study?userId=${userObj.userId}`)).json();
+      } catch (error) {
+        newArray = [
+          { _id: "a", title: "제목 1", details: "설명 1", userIds: ["a", "b", "c"], isLocked: true },
+          { _id: "b", title: "제목 2", details: "설명 2", userIds: ["a", "b", "c", "d", "f"], isLocked: false },
+          { _id: "c", title: "제목 3", details: "설명 3", userIds: ["a", "b", "c", "d"], isLocked: false },
+          { _id: "d", title: "제목 4", details: "설명 4", userIds: ["a", "b", "c"], isLocked: false },
+          { _id: "e", title: "제목 5", details: "설명 5", userIds: ["a", "b", "c"], isLocked: true },
+        ];
+      }
+      setStudyArray(newArray);
+      setLoading(false);
+    };
+    getStudyArray();
+  }, []);
+  if (loading) {
+    return <CardSection>...Loading</CardSection>;
+  }
   return (
-    <div>
+    <div style={{ margin: "80px 0 0 80px", display: "flex" }}>
       <ProfileCard userObj={userObj} />
+      <CardSection>
+        <h2>My Study</h2>
+        <CardList>
+          {studyArray.map((obj) => {
+            return <MyStudyCard key={obj._id} studyObj={obj} />;
+          })}
+        </CardList>
+      </CardSection>
     </div>
   );
 };
