7822f4425ab2172c99eaad007865d00e16c215ff
Harry <haesoo9410@gmail.com>
2021-09-23 22:11:59 +0900

    :art: 유저 객체를 위한 라우트 구조 수정
    유저 데이터를 받아 props로 데이터를 넘길 수 있도록 하였습니다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,45 @@ import SearchStudy from "./pages/SearchStudy";
 import CreateStudy from "./pages/CreateStudy";
 import GroupDetail from "./pages/GroupDetail";
 import FirstPage from "./pages/FirstPage";
+import { useEffect, useState } from "react";
 
 function App() {
-  let isLoggedIn = true; // 로그인  API 받고 나서 바꾸기
+  const [userObj, setUserObj] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUserObj = async () => {
+      let newObj;
+      try {
+        newObj = await (await fetch("/user")).json();
+      } catch (error) {
+        console.error(error);
+        // newObj = { email: "dummy@dummy.com", nickName: "prettyDummy" };
+      }
+      if (newObj) setUserObj(newObj);
+    };
+    getUserObj();
+    setLoading(false);
+  }, []);
+  if (loading) {
+    return <div style={{ fontSize: 40, textAlign: "center", marginTop: 40 + "vh" }}>...loading</div>;
+  }
   return (
     <div className="App">
-      {isLoggedIn ? (
+      {userObj ? (
         <Switch>
-          <Route path="/" exact={true} component={Main} />
-          <Route path="/search" exact={true} component={SearchStudy} />
-          <Route path="/study/create" exact={true} component={CreateStudy} />
-          <Route path="/study/:id" exact={true} component={GroupDetail} />
+          <Route path="/" exact={true} userObj={userObj}>
+            <Main />
+          </Route>
+          <Route path="/search" exact={true} userObj={userObj}>
+            <SearchStudy />
+          </Route>
+          <Route path="/study/create" exact={true} userObj={userObj}>
+            <CreateStudy />
+          </Route>
+          <Route path="/study/:id" exact={true} userObj={userObj}>
+            <GroupDetail />
+          </Route>
         </Switch>
       ) : (
         <Switch>
