34bf27e5f7eec2343a268dcdc53814ba10edd397
seojin kim <001106ksj@gmail.com>
2021-09-23 17:59:29 +0900

    :sparkles: 검색 결과 쿼리스트링을 값으로 들고 있도록 하기
    useLocation과 URLSearchParams를 사용하여 url을 파싱하고 __에 대한 검색 결과라고 출력

--- frontend/src/pages/SearchStudy.js
+++ frontend/src/pages/SearchStudy.js
@@ -1,10 +1,21 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import { useLocation } from "react-router";
 
 const SearchStudy = () => {
+  const location = useLocation();
+  const query = location.search;
+  const parsedQuery = new URLSearchParams(query);
+  const searchWord = parsedQuery.get("q");
+
   return (
-    <div>
+    <div style={{ marginTop: "50px", marginBottom: "50px" }}>
       <SearchBar />
+      {searchWord ? (
+        <div style={{ marginTop: "20px" }}>
+          {searchWord}에 대한 검색 결과입니다.
+        </div>
+      ) : null}
     </div>
   );
 };
