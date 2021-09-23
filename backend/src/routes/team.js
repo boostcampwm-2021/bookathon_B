70a2629de11563acd90bb6f922ea6f46be765d59
gidskql6671 <gidskql6671@gmail.com>
2021-09-24 01:58:39 +0900

    ✨ 요청받은 그룹의 모든 참가자의 당일 커밋수를 보내주는 api 제작
    `GET /study/:teamId/commits`로 오쳥을 보낼 수 있다.
    `teamId` 그룹의 참가자들의 당일 커밋수를 담은 배열을 json으로 보내준다.
    
    json 형식은 다음과 같다.
    `[ { userId: String, commit: Integar } ]`
    - userId : 참가자의 githubId
    - commit : 해당 유저의 당일 커밋 수

--- backend/src/routes/team.js
+++ backend/src/routes/team.js
@@ -1,5 +1,7 @@
 const {createTeam, deleteTeam, updateTeam, searchTeams, enterTeam, exitTeam} = require("../services/team/team");
 const express = require('express');
+const githubService = require('../services/github.js');
+
 const router = express.Router();
 
 router.get('/',searchTeams);
@@ -13,5 +15,13 @@ router.put('/exit', exitTeam);
 router.put('/:teamId',updateTeam);
 
 router.delete('/:teamId',deleteTeam);
+
+router.get('/:teamId/commits', async (req, res) => {
+    const teamId = req.params.teamId;
+    
+    const data = await githubService.getCommitCountOfTeam(teamId);
+    
+    res.json(data);
+})
 
 module.exports = router;
