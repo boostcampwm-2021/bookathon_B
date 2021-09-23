5043d116b78486ca0bb4eec03742c023ac29cd1c
gidskql6671 <gidskql6671@gmail.com>
2021-09-23 14:16:45 +0900

    🏗 백엔드 프로젝트 구조 설립

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./src/routes/index');
+const usersRouter = require('./src/routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
