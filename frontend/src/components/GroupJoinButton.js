3da08029a8b6aa2755ad892b7ee756a31c34f7f5
seojin kim <001106ksj@gmail.com>
2021-09-23 22:31:00 +0900

    :sparkles: 모달 띄우기
    private 여부에 따라 모달을 띄운다

--- frontend/src/components/GroupJoinButton.js
+++ frontend/src/components/GroupJoinButton.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Swal from "sweetalert2";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import withReactContent from "sweetalert2-react-content";
 
 const Button = styled.div`
   display: flex;
@@ -18,9 +20,38 @@ const Button = styled.div`
   }
 `;
 
-const GroupJoinButton = () => {
+const GroupJoinButton = (props) => {
+  const MySwal = withReactContent(Swal);
+  function showPrivateJoinModal() {
+    MySwal.fire({
+      title: "비밀번호를 입력하세요",
+      input: "password",
+      showCancelButton: true,
+      preConfirm: (pw) => {
+        if (pw === props.password) {
+          // 가입 성공
+        } else {
+          MySwal.showValidationMessage(`비밀번호가 잘못되었습니다.`);
+        }
+      },
+    });
+  }
+  function showPublicJoinModal() {
+    MySwal.fire({
+      title: "스터디에 가입하시겠습니까?",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // 가입 성공
+      }
+    });
+  }
   return (
-    <Button>
+    <Button
+      onClick={props.isPrivate ? showPrivateJoinModal : showPublicJoinModal}
+    >
       <AiOutlinePlusCircle width="24" color="#6BD746" />
       추가
     </Button>
