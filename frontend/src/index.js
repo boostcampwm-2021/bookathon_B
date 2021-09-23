617ab35b21f0035d8a5820eee109d2febace718d
Harry <haesoo9410@gmail.com>
2021-09-23 16:56:19 +0900

    :lipstick: global style 추가
    reset css를 글로벌 스타일로 추가함

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./GlobalStyles";
 
 ReactDOM.render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
