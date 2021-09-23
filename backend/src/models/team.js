28cd21f309212f265056aaa8bc6ba96b0428a328
이찬호 <chanholee275@gmail.com>
2021-09-23 16:06:41 +0900

    🗃️ Add:스터디 그룹의 세부사항을 저장할 column 추가

--- backend/src/models/team.js
+++ backend/src/models/team.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const teamSchema = new Schema({
-        teamId: Number,
         title: String,
         password: String,
+        details: String,
         userIds: Array,
 });
 
