de5e2c7abeeee2d24aa02cccb7296545c073c4a1
Harry <haesoo9410@gmail.com>
2021-09-23 19:52:13 +0900

    :sparkles: 로그인 안된 첫 페이지 추가
    로그인 하지 않았을 때 뜨는 첫 페이지와 로고 추가

--- frontend/src/pages/FirstPage.js
+++ frontend/src/pages/FirstPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import MainLogo from "../components/MainLogo";
+import { AiFillGithub } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+const LoginBtn = styled(Link)`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 50px;
+  border-radius: 50px;
+  font-size: 15px;
+  border-style: none;
+  background-color: #68a375;
+  color: white;
+`;
 
 const FirstPage = () => {
-  return <div>first</div>;
+  return (
+    <div>
+      <MainLogo />
+      <LoginBtn to="/signin">
+        <AiFillGithub size="24" />
+        Login With Github
+      </LoginBtn>
+    </div>
+  );
 };
 
 export default FirstPage;
