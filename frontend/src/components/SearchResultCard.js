4a795af68eb2471de818fbe588e4c478b90ed12a
seojin kim <001106ksj@gmail.com>
2021-09-23 21:46:58 +0900

    :lipstick: 검색 결과 컴포넌트 만들기
    버튼은 따로 컴포넌트로 뺌!

---
+++ frontend/src/components/SearchResultCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import GroupJoinButton from "./GroupJoinButton";
+import { AiFillLock } from "react-icons/ai";
+import { BsPeople } from "react-icons/bs";
+import styled from "styled-components";
+
+const TitleText = styled.div`
+  color: #5581c3;
+  font-weight: 800;
+  font-size: 22px;
+  margin-bottom: 14px;
+`;
+
+const SubText = styled.div`
+  color: #999898;
+  font-size: 16px;
+`;
+
+const CardWrapper = styled.div`
+  width: 600px;
+  text-align: left;
+  padding: 24px;
+  border-bottom: 1px solid #999898;
+  position: relative;
+`;
+
+const SearchResultCard = (props) => {
+  return (
+    <CardWrapper>
+      <TitleText>{props.data.title}</TitleText>
+      <SubText>{props.data.description}</SubText>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: "12px",
+          gap: "6px",
+        }}
+      >
+        <BsPeople size="24" />
+        <SubText>{props.data.count}</SubText>
+        {props.data.isPrivate ? <AiFillLock size="24" /> : null}
+      </div>
+      <GroupJoinButton />
+    </CardWrapper>
+  );
+};
+
+export default SearchResultCard;
