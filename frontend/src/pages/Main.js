1fc285627c13109c5078b84481b3323550d262bf
Harry <haesoo9410@gmail.com>
2021-09-24 00:13:57 +0900

    :lipstick: 프로필 카드 레이아웃
    프로필 카드 스타일 추가

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ProfileCard from "../components/ProfileCard";
 
-const Main = () => {
-  return <div>main</div>;
+const Main = ({ userObj }) => {
+  return (
+    <div>
+      <ProfileCard userObj={userObj} />
+    </div>
+  );
 };
 
 export default Main;
