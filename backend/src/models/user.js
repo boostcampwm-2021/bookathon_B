117d998ab868c29921d3ecb85a129b455c34ac89
gidskql6671 <gidskql6671@gmail.com>
2021-09-23 21:15:26 +0900

    :poop: 테스팅을 위해 임시로 스키마를 변경함
    추후 수정해주세요...

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
         email: String,
         teamIds: Array,
         oauthToken : String,
+        githubId: String
 });
 
 module.exports = mongoose.model('User', userSchema);
