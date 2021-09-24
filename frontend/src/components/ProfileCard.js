b1f0400174f0084fb9f93153be9edeeeec77cfc5
Harry <haesoo9410@gmail.com>
2021-09-24 11:01:34 +0900

    :sparkles: 로그아웃 버튼 추가 및 api연결
    프로필 사진 하단에 로그아웃 버튼을 추가하고 api를 연결함

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,15 +1,13 @@
 import React from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { ImCancelCircle } from "react-icons/im";
 
 const Card = styled.div`
   display: inline-block;
-  img {
-    width: 250px;
-    height: 250px;
-    border-radius: 250px;
-    margin-bottom: 15px;
-  }
+
   h2 {
     text-align: left;
     font-size: 25px;
@@ -34,10 +32,50 @@ const Card = styled.div`
     line-height: 30px;
   }
 `;
+const ProfileImg = styled.div`
+  position: relative;
+  img {
+    width: 250px;
+    height: 250px;
+    border-radius: 250px;
+    margin-bottom: 15px;
+  }
+`;
+
+const LogoutBtn = styled(ImCancelCircle)`
+  position: absolute;
+  bottom: 10px;
+  right: 0;
+  fill: rgba(255, 70, 70, 0.7);
+  font-size: 20px;
+  cursor: pointer;
+`;
+
 const ProfileCard = ({ userObj }) => {
+  const onClickLogout = () => {
+    const MySwal = withReactContent(Swal);
+    MySwal.fire({
+      title: "로그아웃 하시겠습니까?",
+      // text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch("auth/logout", {
+          method: "POST",
+        });
+      }
+    });
+  };
   return (
     <Card>
-      <img src={`https://github.com/${userObj.userId}.png`} />
+      <ProfileImg>
+        <img src={`https://github.com/${userObj.userId}.png`} />
+        <LogoutBtn onClick={onClickLogout} />
+      </ProfileImg>
       <h2>{userObj.nickName}</h2>
       <h3>{userObj.userId}</h3>
       <Link to="/search">Create Study</Link>
