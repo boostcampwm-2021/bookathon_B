5d5dfa01e71812d11e914496f679ef958890b737
seojin kim <001106ksj@gmail.com>
2021-09-24 11:54:17 +0900

    :sparkles: 그룹 상세 스터디원별 커밋수 퍼블리싱

--- frontend/src/pages/GroupDetail.js
+++ frontend/src/pages/GroupDetail.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import GroupCommitTable from "../components/GroupCommitTable";
 
 const GroupDetail = () => {
-  return <div>detail</div>;
+  const { id } = useParams();
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <div>{id}의 커밋 현황</div>
+      <GroupCommitTable groupId={id} />
+    </div>
+  );
 };
 
 export default GroupDetail;
