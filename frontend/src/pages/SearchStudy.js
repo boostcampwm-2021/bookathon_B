fb9887229441afc9e173d21094e865ca9b9ffac0
gidskql6671 <gidskql6671@gmail.com>
2021-10-02 19:23:33 +0900

    ✨ 서버로부터 스터디방 데이터를 받아오도록 구현

--- frontend/src/pages/SearchStudy.js
+++ frontend/src/pages/SearchStudy.js
@@ -12,58 +12,13 @@ const SearchStudy = () => {
   const [title, setTitle] = useState("");
   useEffect(() => {
     function getResult() {
-      const result = [
-        // 여기 나중에 API 호출하기!
-        {
-          _id: 1,
-          title: "잔디잔디",
-          description: "잔디를 길러봐",
-          isPrivate: true,
-          count: 3,
-          password: "12345",
-        },
-        {
-          _id: 2,
-          title: "잔디잔디잔디",
-          description: "잔디를 길러봐?",
-          isPrivate: false,
-          count: 3,
-          password: null,
-        },
-        {
-          _id: 3,
-          title: "잔디잔",
-          description: "잔디를 길러봐앙",
-          isPrivate: true,
-          count: 3,
-          password: "12345",
-        },
-        {
-          _id: 4,
-          title: "잔잔디디",
-          description: "잔디를 길러봐아아?",
-          isPrivate: false,
-          count: 3,
-          password: null,
-        },
-        {
-          _id: 5,
-          title: "디잔디",
-          description: "잔디를 길러봐ba",
-          isPrivate: true,
-          count: 3,
-          password: "12345",
-        },
-        {
-          _id: 6,
-          title: "잔잔잔",
-          description: "잔디를 길러봐bwa?",
-          isPrivate: false,
-          count: 3,
-          password: null,
-        },
-      ];
-      setSearchResult(result);
+      fetch(`/study?title=${title}`)
+      .then(res => res.json())
+      .then(data => {
+        const studys = data.study;
+        
+        setSearchResult(studys);
+      });
     }
     getResult();
   }, [title]);
